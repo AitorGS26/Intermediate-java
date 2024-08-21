c6aed7cb0ec42492aa76771e1294df18684a58dd
ersi <empirecodingfull@gmail.com>
2024-08-21 21:12:54 +0200

    adding the introduction of inheritance for following next day

---
+++ Intermediate-java-lessons-I/i_OOP_Inheritance.java
@@ -0,0 +1,39 @@
+/* Inheritance is a word that I've been hearing since I started learning java
+ today is the day we'll learn what is this and for what is used.
+
+ ** Inheritance in java is a process for reusing code when we create new classes
+ 
+ *** What we do is to indicate to the program that we want to use the variables 
+ and functions os a previously created class
+ *** In other words, we can use the methods and atributes of a class that exists previously
+ and put them inside a new class without writing the same code again
+ 
+ **ATRIBUTES == VARIABLES
+ **METHODS == THE FUNCTIONS OF A CLASS
+ 
+ * The inheritance is a huge advantage, the programmer will use less code and less time for 
+ the same work
+ 
+ When we use the inheritance, there are two terms really important on this:
+ 
+  ** The "father" class, or base class => Here is the code that we'll use in the future, the
+  atributes and the methods, here we declare all the variables and methods which are going 
+  to make some parts of the program work.
+
+  ** The "son" class, or derived class => This is the class that will "inherit" the atributes
+  and methods, that were createn on the father class. Here those elements will be used without 
+  writing them again 
+  
+  ** In programming there are TWO tipes of inheritance:
+   - Simple inheritance => THE ONLY ONE WE USE IN JAVA
+     A son class will only have ONE father class, but the father class can have more than
+     one son class
+
+   - Multiple inheritance => WE DON'T USE THIS IN JAVA
+   
+   **IN CONCLUSION => Inheritance is the way tu reuse the previously creted code, for not writing it
+   one and again.*/
+
+public class i_OOP_Inheritance {
+    
+}
