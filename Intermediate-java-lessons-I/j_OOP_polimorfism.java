2a8cf7c70d09871ffb15fc18795a787f9a30a6df
ersi <empirecodingfull@gmail.com>
2024-08-22 18:24:50 +0200

    Polymorfism concept's theory and a proyect for a better understanding

---
+++ Intermediate-java-lessons-I/j_OOP_polimorfism.java
@@ -0,0 +1,17 @@
+/*
+ * Polymorfism in OOP, is to give a method the capacity of have different behaviour 
+ depending on the created instance.
+
+ * For example => Supose someone called Paco is a method and he receives two calls.
+ * on the one side if the call is fot getting a job or from his bosses Paco will have a formal behaviour.
+ * on the other side if the call is from a friend, Paco will have an informal behaviour.
+ * 
+ * In conclusion the polimorfismconsists on comunicating one method with the different clasess
+ and depending on with which have the comunication, it's behaviour will be one or other.
+
+ For a better understanding, there will be a project called "j_OOP_polimorfism"
+ */
+
+public class j_OOP_polimorfism {
+    
+}
---
+++ Intermediate-java-lessons-I/k_OOP_super_reservedWord.java
@@ -0,0 +1,11 @@
+/*
+ * "super" reserved word is used for havin access to an element on the father class.
+ * 
+ * one of the most used uses for super word, is to call the fathes's class constructor.
+ * Thank to that, we'll reduce the amount of lines for our doughter clasess.
+ * 
+ * 
+ */
+public class k_OOP_super_reservedWord {
+    
+}
