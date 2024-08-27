4cb4b0d97f738f23effc3821ee2868f280aa992f
ersi <empirecodingfull@gmail.com>
2024-08-27 05:20:46 +0200

    first steps into syncronizations in threads

--- Intermediate-java-lessons-I/q_Threads_states.java
+++ Intermediate-java-lessons-I/q_Threads_states.java
@@ -1,5 +1,5 @@
 /*A thread has different states from it's creation, those are:
- * 
+  
  * New                     => The thread has been created but is not executed the start() method.
                            There will appear an error message if you try to execute any method 
                            without start() method
@@ -8,9 +8,8 @@
  * Blocked or not runnable => The thread is executing but there is one job or activity that blocks
  * Dead                    => A thread can dead by two ways, natural dead, which occurs when
                            it's run() method finishes normally, the other way will be a murder,
-                           it occurs when there is an instruction to finish the thread before it finishes
-
- */
+                           it occurs when there is an instruction to finish the thread before it 
+                           finishes*/
 
 public class q_Threads_states {
 
---
+++ Intermediate-java-lessons-I/r_threac_sync.java
@@ -0,0 +1,12 @@
+/*Thread sincronization allow the control of time and for on different theads runnin simultaneously 
+ * 
+ * The objective is to avoid the posibility of one thread bother another thread's behaviour.
+  
+ * Also for stablish and order of execution in our program using threads
+ * 
+ * It looks like it's easy, but for more understanding, there will be a project
+ called r_thread_sync_project, where we'll work with 4 threads simmultaneously and sincronyzed.*/
+
+public class r_threac_sync {
+
+}
